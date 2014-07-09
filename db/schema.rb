7f2fd583d03a5953a05536c7ccd005f35f7347da
remy <rrojasdela92@gmail.com>
2014-07-09 18:22:20 +0200

    intento de migrar db desde pgadmin

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140709160613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "Libro_Diario", id: false, force: true do |t|
+    t.date    "Fecha",                                null: false
+    t.text    "Tipo_de_Cambio",                       null: false
+    t.text    "Cuenta",                               null: false
+    t.float   "Deber",                                null: false
+    t.float   "Haber",                                null: false
+    t.text    "Glosario"
+    t.boolean "Transaccion_Obsoleta", default: false, null: false
+    t.integer "Tipo_de_Cuenta",                       null: false
+    t.integer "ID",                                   null: false
+  end
+
+  create_table "Plan_de_Cuentas", id: false, force: true do |t|
+    t.text "Descripcion"
+    t.text "Codificacion", null: false
+  end
+
+  create_table "trecarga_developments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
