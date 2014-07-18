296882989b182eac5f38abeb112243c9bb067bba
remy <rrojasdela92@gmail.com>
2014-07-18 17:48:14 +0200

    sign in finalizado

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	def new
+		render 'new'
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = 'Usuario o Password incorrectos'
+			render 'new'
+		end
+	end
+
+	def destroy
+	  sign_out
+	  redirect_to root_url
+	  
+	end
+
+end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-module SessionHelper
+module SessionsHelper
 	
 	def sign_in(user)
 		remember_token = User.new_remember_token
@@ -18,5 +18,12 @@ module SessionHelper
 	
 	def signed_in?
 		!current_user.nil?
+	end
+	
+	def sign_out
+	  current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
 	end
 end
